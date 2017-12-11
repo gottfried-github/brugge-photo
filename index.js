5771c7c82e44e17828fec1310c63797ab7665260
Hryts Jakovytch Varenyk <wsa@localhost.localdomain>
2017-12-11 18:59:24 +0200

    stable checkpoint;

--- index.js
+++ index.js
@@ -27,20 +27,123 @@ var Index = {
 
     for (var i = 0; i < gridItems.length; i++) {
       // var itemWidth = ( ParseInt($(grid).innerWidth()) / 100) * itemsWidths[i] - margin * 2;
-      gridItems[i].style.width = itemsWidths[i] + "%";
+      gridItems[i].style.width = this.itemsWidths[i] + "%";
     }
   },
   modernize: function() {
     // photo slides
     if (!Modernizr.cssvhunit) {
+      consoloe.log('falling back vh')
       $('.photoSlides').css('height', $(window).height());
     }
   }
 }
 
+var Slider = {
+  slots: {
+    a: $('.photo-top'),
+    b: $('.photo-bottom')
+  },
+  stack: {
+    top: $('.photo-top'),
+    bottom: $('.photo-bottom')
+  },
+  slideUrls: [
+    'photos/02-21_20140108170653_3959947_large.jpg',
+    'photos/21_20131213163318_3929317_large.jpg',
+    'photos/21_20131213163343_3929319_large.jpg',
+    'photos/21_20101122202029_1047854_large.jpg',
+    'photos/21_20131213163327_3929318_large.jpg'
+  ],
+  paused: false,
+  toggleSlide: function() {
+    console.log('toggleSlide')
+    if (this.paused) {
+      this.paused = false;
+      this.slide();
+    } else if (!this.paused) {
+      this.paused = true;
+    }
+  },
+  subscribeToggle: function() {
+    var self = this;
+    $('.photoSlides').on('click', function() {
+      self.toggleSlide()
+    })
+    console.log('sub toggle, ', this.toggleSlide)
+  },
+  init: function(period) {
+    this.period = period;
+    this.slots.a.addClass('ztop');
+    this.slots.a.addClass('transition');
+
+    this.slideUrls.push(this.slideUrls.shift());
+    this.slideUrls.push(this.slideUrls.shift());
+
+    this.subscribeToggle();
+    var self = this;
+    window.setTimeout(function() {
+      self.slide();
+    }, self.period);
+  },
+  slide: function() {
+    var self = this;
+    // var cb = function(callback) {
+    //   window.setTimeout(function() {
+    //      console.log(self)
+    //      self.doSlide(top, bottom, callback)
+    //    }, 1500)
+    //   }
+    // }
+    this.doSlide(this.stack.top, this.stack.bottom, function(top, bottom, cb) {
+     window.setTimeout(function() {
+       console.log(self)
+       console.log(this)
+       if (!self.paused) {
+         self.doSlide(top, bottom, cb)
+       } else if (self.paused) {
+         self.stack.top = top;
+         self.stack.bottom = bottom;
+       }
+     }, self.period)
+    })
+  },
+  doSlide: function(top, bottom, cb) {
+    var self = this;
+    top.addClass('transparent');
+    top.on('transitionend', function() {
+      // prepare the slot to be the bottom next time
+      $(this).removeClass('transition');
+      $(this).removeClass('ztop');
+
+      // put the bottom slide on the top
+      bottom.addClass('ztop');
+      bottom.addClass('transition');
+
+      // bottom is now at the top, so we can make the latest top slot rigid
+      $(this).removeClass('transparent');
+      $(this).off('transitionend');
+
+      var image = self.slideUrls.shift();
+      $(this).css('background-image', 'url('+ image +')');
+      self.slideUrls.push(image);
+      console.log(cb)
+      cb(bottom, top, cb)
+      // self.stack.top = bottom;
+      // self.stack.bottom = top;
+    })
+  }
+
+    /*
+
+    */
+
+}
+
 function initIndex() {
   Index.gridSetup();
   Index.modernize();
+  Slider.init(2850);
 }
 
-$(document).load(initIndex)
+$(document).ready(initIndex)
